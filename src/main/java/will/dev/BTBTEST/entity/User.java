85105f70f68c88a11b322f14849efcc93cdc35a0
William Ndongmo <williamndongmo88@gmail.com>
2025-04-22 16:31:34 +0400

    Migration des données avec Flyway, gestion des profils

--- src/main/java/will/dev/BTBTEST/entity/User.java
+++ src/main/java/will/dev/BTBTEST/entity/User.java
@@ -5,6 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Getter
@@ -12,7 +20,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +34,47 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+
+    private LocalDate createDay;
+
+    // Méthode pour définir automatiquement le jour de la creation du compte
+    @PrePersist
+    public void prePersist() {
+        this.createDay = LocalDate.now(ZoneId.systemDefault());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + this.role.getLibelle()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.mdp;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
---
+++ src/main/resources/db/migration/V1.3__add-create_day-to-user.sql
@@ -0,0 +1,1 @@
+ALTER TABLE users ADD create_day TIMESTAMP;
